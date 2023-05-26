2e73b08033f9b687d7f17a10dec733dae5ded280
samobrien <sam.obrien@hertz.com>
2023-05-26 12:06:25 +0100

    all_possible_lines method empty

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,31 @@
+from typing import List
 
 def play():
     pass
+
+
+def get_all_possible_lines(rows: List[List[str]]):
+    """
+    For the given game state, represented by a list o
+    :param game_board:
+    :return:
+    """
+    all_possible_lines: List[List[str]] = [][]
+
+    # Get all rows
+    for row in rows:
+        all_possible_lines.append(row.copy())
+
+
+    # Add all columns
+
+
+    # Add all diagonals
+
+
+    assert len(all_possible_lines) == 8
+    return all_possible_lines
+
 
 if __name__ == '__main__':
     play()
